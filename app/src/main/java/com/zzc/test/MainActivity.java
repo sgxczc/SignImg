f57fd548367660f8c061010f981bf874ab305592
sgxczc <sgxczc@163.com>
2020-12-02 11:12:15 +0800

    测试

--- app/src/main/java/com/zzc/test/MainActivity.java
+++ app/src/main/java/com/zzc/test/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("ADAC","测试");
+        Log.e("ADAC","测试是");
     }
 }
