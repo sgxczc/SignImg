6dcd047afc03cf18d17a88bec884fa4cd85b2a63
sgxczc <sgxczc@163.com>
2021-07-17 10:50:10 +0800

    测试

--- app/src/main/java/com/zzc/signImg/MainActivity.java
+++ app/src/main/java/com/zzc/signImg/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mSignaturePad.clear();
             }
-        }); 
+        });
 
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
